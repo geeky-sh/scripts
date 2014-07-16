359ef413477edddc38d9aaddaa22379a90c91af3
geeky-sh <aash.discover@gmail.com>
2014-07-16 20:28:34 +0530

    making minor changes to make the battery script to work

--- battery-low-notify.rb
+++ battery-low-notify.rb
@@ -4,11 +4,11 @@ client = Washbullet::Client.new('v1fjkeba2rEECQIXT571Wi5QRKlAYIYNreujydPjC7FPo')
 iden = 'ujydPjC7FPodjzWIEVDzOK'
 
 percent = `cat /sys/class/power_supply/BAT0/capacity`.strip.to_i
-status = `cat /sys/class/power_supply/BAT0/status`
+status = `cat /sys/class/power_supply/BAT0/status`.strip
 
 if percent < 5 && status == 'Discharging'
 	client.push_note(iden, "Arch Battery Status < 5%.", " The system will now be suspended. Please Plug the charger soon")
 	`sudo pm-suspend`
-elsif status < 15 && status == 'Discharging'
+elsif percent < 15 && status == 'Discharging'
 	client.push_note(iden, "Arch Battery Status", "Battery Low: #{percent}%. Please connect the charger")
 end
